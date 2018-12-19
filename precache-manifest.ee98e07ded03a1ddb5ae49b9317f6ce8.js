36bcb6000ebe3b22fc583cdf03fa4304c27c97da
Salman Ahmed Abbasis <SalmanAhmed24@users.noreply.github.com>
2018-12-19 16:56:01 +0500

    Updates

---
+++ precache-manifest.ee98e07ded03a1ddb5ae49b9317f6ce8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4306f9a417754a64c28ba1821f37c31f",
+    "url": "/mobcom/static/media/car.4306f9a4.png"
+  },
+  {
+    "revision": "5d5f31e1b5b838c2aebd",
+    "url": "/mobcom/static/css/main.cd07ebb5.chunk.css"
+  },
+  {
+    "revision": "bde68ee6bd9549362f88690209b7edb8",
+    "url": "/mobcom/static/media/free.bde68ee6.jpg"
+  },
+  {
+    "revision": "eb9ed15cf54aabef5d73",
+    "url": "/mobcom/static/js/1.eb9ed15c.chunk.js"
+  },
+  {
+    "revision": "fe007ffb14c871a9af45",
+    "url": "/mobcom/static/js/runtime~main.fe007ffb.js"
+  },
+  {
+    "revision": "454a671a1c57206e6e0b6f55c8e05188",
+    "url": "/mobcom/static/media/back.454a671a.jpg"
+  },
+  {
+    "revision": "5d5f31e1b5b838c2aebd",
+    "url": "/mobcom/static/js/main.5d5f31e1.chunk.js"
+  },
+  {
+    "revision": "8a40bebf1c6632522ae3f3d4aa6415bb",
+    "url": "/mobcom/static/media/arooj.8a40bebf.png"
+  },
+  {
+    "revision": "03758eba32b897b64bb770b7e6a97396",
+    "url": "/mobcom/static/media/amri.03758eba.png"
+  },
+  {
+    "revision": "0b1f48ba305546a15bca5e713aa83eed",
+    "url": "/mobcom/static/media/react.0b1f48ba.png"
+  },
+  {
+    "revision": "eb9ed15cf54aabef5d73",
+    "url": "/mobcom/static/css/1.9925944c.chunk.css"
+  },
+  {
+    "revision": "f39587b56e1b7f238d05fb206fb4db99",
+    "url": "/mobcom/index.html"
+  }
+];
